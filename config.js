20573f02877e846eac333baa03af27f7658f400e
sebastiendesouza-tech <sebastiendesouza@free.fr>
2026-07-04 10:18:04 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.LOTO_CONFIG = {
   SUPABASE_URL: "https://vexpdodlhnoorsoektmn.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_D4INnFIdG-Mr4EtC9wT7Lw__-TykxNV"
+  SUPABASE_ANON_KEY: "sb_publishable_D4INnFIdG-Mr4EtC9wT7Lw__-TykxNV",
+  TEAM_PIN: "2580"
 };
